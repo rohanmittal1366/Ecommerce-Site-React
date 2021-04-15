305a837851b12f03739b7580e533a7ab18b38fa0
rohan_mittal <rohanmittal1366@gmail.com>
2021-04-16 01:19:21 +0530

    add object using useState

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../directory/Directory";
+import "./homepage_style.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;

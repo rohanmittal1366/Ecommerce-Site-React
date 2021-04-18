8c4d5af3b89a49457edec06f1e09cb35f8efcf19
rohan_mittal <rohanmittal1366@gmail.com>
2021-04-18 16:53:01 +0530

    add firebase

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from "../Firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const Config = {
+  apiKey: "AIzaSyDvvM7W2CLfBEvCWEQjq5nPd04J5hEg9Ag",
+  authDomain: "e-commerce-site-5f2ca.firebaseapp.com",
+  projectId: "e-commerce-site-5f2ca",
+  storageBucket: "e-commerce-site-5f2ca.appspot.com",
+  messagingSenderId: "102365553808",
+  appId: "1:102365553808:web:886684cf211160aa79e415",
+  measurementId: "G-P4G11SZKFY",
+};
+
+firebase.intializeApp(config);

78ff9289f896f87deb69d800492ff627d8ed2260
rohan_mittal <rohanmittal1366@gmail.com>
2021-05-26 15:26:29 +0530

    update cartIcon to access the quantity of cardItems

--- src/Components/cart-icon/CartIcon.js
+++ src/Components/cart-icon/CartIcon.js
@@ -4,11 +4,11 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../Redux/cart/cart.actions";
 import { connect } from "react-redux";
 
-function CartIcon({ toggleCartHidden }) {
+function CartIcon({ toggleCartHidden, itemCount }) {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 }
@@ -17,4 +17,11 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumatedQuantity, cartItem) => accumatedQuantity + cartItem.quantity,
+    0
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);

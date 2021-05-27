88aa66745212258cfd531fbabefc19680b24dee2
rohan_mittal <rohanmittal1366@gmail.com>
2021-05-27 22:38:35 +0530

    added nested router in shop page

--- src/Components/shoppage/ShopPage.js
+++ src/Components/shoppage/ShopPage.js
@@ -1,9 +1,12 @@
+import { Route } from "react-router";
+import Category from "../../pages/category/Category.js";
 import CollectionOverview from "../collection-overview/CollectionOverview.js";
 
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ match }) => {
   return (
     <div className="Shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:categoryId`} component={Category} />
     </div>
   );
 };
---
+++ src/pages/category/Category.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./category.scss";
+
+const Category = ({ match }) => {
+  return (
+    <div className="category">
+      <h2>CATEGORY PAGE</h2>
+    </div>
+  );
+};
+
+export default Category;
